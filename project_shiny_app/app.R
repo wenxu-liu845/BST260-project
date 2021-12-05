d406fd67b7bd93186b3624e71b82953e9a5460e2
Xinyun Li <stacyli@dhcp-10-250-225-125.harvard.edu>
2021-12-05 13:12:16 -0500

    stacy updated

---
+++ project_shiny_app/app.R
@@ -0,0 +1,76 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(tidyverse)
+library(forcats)
+library(dslabs)
+library(ggplot2)
+bigtable<-read.csv("bigtable.csv")
+
+ui <- fluidPage(
+   theme = shinythemes::shinytheme("darkly"),
+   # Application title
+   titlePanel("Final Project Shiny App"),
+   
+   tabsetPanel(
+     tabPanel("Total case vs. date",
+   # Sidebar with a slider input for number of bins 
+        sidebarLayout(
+          sidebarPanel(
+            sliderInput("dates",
+                     "Dates:",
+                     min = as.Date("2021-01-01"),
+                     max = as.Date("2021-11-30"),
+                     value = c(as.Date("2021-02-01"),as.Date("2021-10-01"))
+                     #value=c(min,max)
+                     ),
+            selectInput("state1",label="Select a state",
+                        choices = as.list(levels(bigtable$state))
+                        ),
+            selectInput("state2",label="Select a state",
+                        choices = as.list(levels(bigtable$state))
+                        )
+          ),
+      
+      # Show a plot of the generated distribution
+          mainPanel(
+            plotOutput("line1")
+          )
+        )#sidebarLayout
+     ),
+    tabPanel("Vaccine",
+      sidebarLayout(
+        sidebarPanel(
+          
+        ),#sidebarPanel
+        mainPanel(
+          plotOutput("line2")
+        )
+      )#sidebarlayout
+    )#tabpanel
+   )#tabsetPanel
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+   output$line1 <- renderPlot({
+      bigtable%>%
+       filter(date >= min(input$dates) & date <= max(input$dates))%>%
+       filter(state %in% c(input$state1,input$state2))%>%
+       ggplot(aes(date,tot_case,color=state))+
+       geom_line()+
+       xlab("Dates") +
+       ylab("Total cases")
+   })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
